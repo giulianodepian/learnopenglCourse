dd580765d2ab25c57b50cae4e27d0432aaac20f3
Giuliano De Pian <giulianodepian97@gmail.con>
2025-09-02 22:21:34 -0300

    Updated Vertex

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -56,9 +56,10 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
+        // Coords           // Colors
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left 
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top   
     };
 
     unsigned int vboId, vaoId;
@@ -70,8 +71,11 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vboId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
