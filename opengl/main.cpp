2ee8792aaff3666389534eae773a94c71c9562f6
Giuliano De Pian <giulianodepian97@gmail.con>
2025-09-03 22:41:03 -0300

    Added offset uniform to Vertex Shader

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -104,6 +104,7 @@ int main()
         // draw our first triangle
         shaderProgram.use();
         glBindVertexArray(vaoId); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        shaderProgram.setFloat("offset", 0.3f);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
 
