aa1038d61c738aa81cc7a8b203cf0bc4a806d60f
Giuliano De Pian <giulianodepian97@gmail.con>
2025-09-03 22:48:47 -0300

    Edited Vertex Shader

--- opengl/fragment.fs
+++ opengl/fragment.fs
@@ -1,7 +1,7 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 aVertexColor;
+in vec3 aVertexPos;
 void main()
 {
-   FragColor = vec4(aVertexColor, 1.0f);
+   FragColor = vec4(aVertexPos, 1.0f);
 };
