80e580f0f52ef491eb08c7eb66d39779f71f5320
giulianodepian <giulianodepian97@gmail.com>
2025-11-02 20:20:56 -0300

    Renamed updateCameraVector to updateCameraDirections

--- opengl/camera.h
+++ opengl/camera.h
@@ -46,7 +46,7 @@ public:
         WorldUp = up;
         Yaw = yaw;
         Pitch = pitch;
-        updateCameraVectors();
+        updateCameraDirections();
     }
     // constructor with scalar values
     Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Direction(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Fov(ZOOM)
@@ -55,7 +55,7 @@ public:
         WorldUp = glm::vec3(upX, upY, upZ);
         Yaw = yaw;
         Pitch = pitch;
-        updateCameraVectors();
+        updateCameraDirections();
     }
 
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
@@ -97,7 +97,7 @@ public:
         }
 
         // update Front, Right and Up Vectors using the updated Euler angles
-        updateCameraVectors();
+        updateCameraDirections();
     }
 
     // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
@@ -112,7 +112,7 @@ public:
 
 private:
     // calculates the front vector from the Camera's (updated) Euler Angles
-    void updateCameraVectors()
+    void updateCameraDirections()
     {
         // calculate the new Front vector
         glm::vec3 front;
